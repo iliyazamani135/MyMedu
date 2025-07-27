ebe70ee0d75f39d837a41bcaf6020ac94de9a60a
iliya zamaninezhad <i.zamaninezhad@gmail.com>
2025-07-27 16:47:49 +0330

    Start Project

---
+++ MyMeduDomain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMeduDomain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string NationalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public Enum Role { get; set; }
+        
+      
+      // public bool IsStudent() => Role == Role.Student;
+      // public bool IsTeacher() => Role == Role.Teacher;
+      // public bool IsParent() => Role == Role.Parent;
+    }
+}
